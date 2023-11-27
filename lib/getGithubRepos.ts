4a2eb1f115832045eba23b53c51dccfc0c0f15ed
eins87 <1711520021@student.budiluhur.ac.id>
2023-11-27 18:16:39 +0700

    add revalidate to 3 hours for github request

--- lib/getGithubRepos.ts
+++ lib/getGithubRepos.ts
@@ -1,6 +1,8 @@
 import { GraphQLClient } from "graphql-request"
 import { repos, FavoriteRepos } from "./gqlQueries"
 
+export const revalidate = 60 * 60 * 3 // 3 hours
+
 const graphqlClient = new GraphQLClient(process.env.GITHUB_GRAPHQL_ENDPOINT as string, {
   headers: {
     authorization: `Bearer ${process.env.GITHUB_GRAPHQL_API_KEY as string}`,
